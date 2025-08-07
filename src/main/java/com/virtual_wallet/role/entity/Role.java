80fa5f32b77662376756dfc2d5013b4af001a2e0
Nelson Musto <mustonelson@gmail.com>
2025-08-07 16:32:59 -0300

    feat: add User and Role entity

---
+++ src/main/java/com/virtual_wallet/role/entity/Role.java
@@ -0,0 +1,24 @@
+package com.virtual_wallet.role.entity;
+
+import com.virtual_wallet.common.enums.RoleName;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName name;
+}
---
+++ src/main/java/com/virtual_wallet/user/entity/User.java
@@ -0,0 +1,46 @@
+package com.virtual_wallet.user.entity;
+
+import com.virtual_wallet.role.entity.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastname;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private Double balance = 0.0;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    private boolean enabled = true;
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+}
