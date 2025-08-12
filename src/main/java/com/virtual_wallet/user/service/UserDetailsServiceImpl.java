05670caf7fd344b8a4425f62d2c781db0b9901ff
Nelson Musto <mustonelson@gmail.com>
2025-08-12 20:48:03 -0300

    feat(jwt): add loadUserByUsername method to UserDetailsServiceImp

--- src/main/java/com/virtual_wallet/user/service/UserDetailsServiceImpl.java
+++ src/main/java/com/virtual_wallet/user/service/UserDetailsServiceImpl.java
@@ -4,11 +4,16 @@ import com.virtual_wallet.user.entity.UserEntity;
 import com.virtual_wallet.user.repository.UserRepository;
 import com.virtual_wallet.util.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -26,11 +31,24 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        UserEntity user = userRepository.findUserEntityByEmail(email)
+        UserEntity userFound = userRepository.findUserEntityByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
+        List<SimpleGrantedAuthority> authorities = userFound.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .collect(Collectors.toList());
 
-
-        return null;
+        return new User(
+                userFound.getEmail(),
+                userFound.getPassword(),
+                userFound.isEnabled(),
+                userFound.isAccountNonExpired(),
+                userFound.isCredentialsNonExpired(),
+                userFound.isAccountNonLocked(),
+                authorities
+        );
     }
+
+    
+
 }
