20576f40dbc936d239c26938d43d17f4ea52e20d
Nelson Musto <mustonelson@gmail.com>
2025-09-11 15:59:35 -0300

    feat(permission): add PermissionRespository

---
+++ src/main/java/com/virtual_wallet/permission/repository/PermissionRepository.java
@@ -0,0 +1,9 @@
+package com.virtual_wallet.permission.repository;
+
+import com.virtual_wallet.permission.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+}
