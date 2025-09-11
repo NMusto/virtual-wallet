0d8eb16bfb8b5d844bf07020e64e478a0db38a17
Nelson Musto <mustonelson@gmail.com>
2025-09-11 16:01:30 -0300

    feat(role): add RoleRepository

---
+++ src/main/java/com/virtual_wallet/role/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.virtual_wallet.role.repository;
+
+import com.virtual_wallet.role.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
